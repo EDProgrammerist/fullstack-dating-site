da40542932d29e5fb1a2f57a6c63e063f8047645
edprogrammerist <belseri0n@protonmail.com>
2025-08-21 20:06:14 +0800

    first commmit

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cbupdihzwkppsqoufxzv.supabase.co",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
